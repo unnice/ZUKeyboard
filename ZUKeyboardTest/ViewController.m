74d4f66f4deaca377dbe71848170f6c61581a856
Xapek86 <xapek86@Xapek86s-Mac.local>
2017-08-30 09:06:16 -0700

    Initial Commit

---
+++ ZUKeyboardTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZUKeyboardTest
+//
+//  Created by Xapek86 on 8/30/17.
+//  Copyright © 2017 Xapek86. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ZUKeyboardTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZUKeyboardTest
+//
+//  Created by Xapek86 on 8/30/17.
+//  Copyright © 2017 Xapek86. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
